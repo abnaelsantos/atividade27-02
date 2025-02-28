5068509e5d774b2617d62a7272aa5f74ea725bc0
abnael <abnaellima.senai@gmail.com>
2025-02-28 14:36:16 -0300

    resolvido

--- execcio08.js
+++ execcio08.js
@@ -1,1 +1,11 @@
-var code
+let valorPremio = 5000;
+let quantidadePessoas = 5;
+let valorreceber;
+
+function calcularValorreceber(){
+    var calcularValorreceber = valorPremio / quantidadePessoas;
+    var valorreceber = calcularValorreceber;
+    return valorreceber;
+}
+
+console.log("Cada pessoa irá receber o valor de:", calcularValorreceber(valorreceber)," reias");
